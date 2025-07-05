1e3858235fafddc56d16e71c7614e8be1d5c6908
Alon <ageva10@gmail.com>
2025-07-05 12:49:46 +0300

    first commit

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -24,7 +24,8 @@ class ProjectController extends BaseController {
     try {
       const { page = 1 } = req.query
 
-      await axios.get(`https://gw.yad2.co.il/yad1/projects?topArea=2&area=4&city=7900&limit=200&page=${page}`)
+      const result = await axios.get(`https://gw.yad2.co.il/yad1/projects?topArea=2&area=4&city=7900&limit=200&page=${page}`)
+      console.log(result)
 
       // Petah Tikva
       const { data: result1 } = await axios.get(`https://gw.yad2.co.il/yad1/projects?topArea=2&area=4&city=7900&limit=200&page=${page}`)
